55db8d3ef031167b8147c5bafd632889d43eed6f
syamsulapp <samsul.marif100@gmail.com>
2022-08-27 22:18:32 +0800

    change code

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 const express = require('express')
-const routerUsers = require('./router/rout')
 const app = express()
 const port = 3000
 
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-}) 
-
-app.get('/about', (req, res) => {
-  res.send ( 'apa')
+app.get('/', (req, res)=> {
+  res.send('hello world')
 })
-  
 
-
-app.use(routerUsers)
-
-app.listen(port, () => {
-console.log(`jalan di port ${port}`)
+app.listen(port ,()=> {
+  console.log(`port ${port}`)
 })
