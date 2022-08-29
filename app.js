a3f9f49a334a741fa2d6f91ff589d5d1e097a2e7
uderahman <uderahman2105@gmail.com>
2022-08-29 22:36:26 +0800

    install ejs dan buat haman views, insyallah besok include layout

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const port = 3000
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
 }) 
 
 app.get('/about', (req, res) => { 
