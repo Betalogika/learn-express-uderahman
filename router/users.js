c7e7839303ae5531b808b5ada7800802a2e8308b
uderahman <uderahman2105@gmail.com>
2022-10-14 00:33:21 +0800

    pada file users.js membuat komentar pada baris ke-21 yang berisi deskripsi dan contoh design pattern singleton

--- router/users.js
+++ router/users.js
@@ -16,9 +16,28 @@ router.post('/users', postUsers);
 router.patch('/users/:id', UpdateUsers); 
 router.delete('/users/:id', deletUsers); 
 
-
-
 export default router;
+
+//menggunakan singleton
+        // design pattern merupakan template/pola penulisan code dengan menggunakan koonsep pembuatan objek menggunkan constructor function, namun dibungkus menggunakan class. Berikut jika code dengan pola functional diatas jika diubah menggunakan menjadi pola singleton:
+
+// class methodroute extends Express.Router {
+//     Method ( getproduk,
+//         postProduk,
+//         UpdateProduk,
+//         deletProduk,
+//         getById,
+//         home) {
+//  this.geth = router.get ('/', home);
+//  this.getp = router.get('/produk/juragan_ude', getproduk);
+//  this.getid = router.post('/Produk/juragan_ude', postProduk);
+//  this.post = router.post('/Produk/juragan_ude', postProduk);
+//  this.patc = router.patch('/produk/juragan_ude/:id', UpdateProduk);
+//  this.delete = router.delete('/produk/juragan_ude/:id', deletProduk)
+//     }
+// }
+
+// export default methodroute;
 
 // dengan menggunakan principle OOP
 // class usersroute extends express.Router{
@@ -28,12 +47,12 @@ export default router;
 //             router.get('/users/:id', getById),
 //             router.post('/users', postUsers),
 //             router.patch('/users/:id', UpdateUsers),
-//             router.delete('/users/:id', deletUsers)
+//             router.deletee('/users/:id', deletUsers)
               
 //         ]
 //     }
 // }
-// export default router;
+// export default usersroute;
 
 
 
